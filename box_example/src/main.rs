2d0da20bfea8b785da63575b9ead7942cc5a12d3
zhaoyanximeng <1368788830@qq.com>
2023-03-20 05:36:21 +0800

    Rc示例

--- box_example/src/main.rs
+++ box_example/src/main.rs
@@ -1,6 +1,27 @@
+/*// Box
 fn main() {
     let a = 5;
     let b = Box::new(a);
     println!("{}", a);
     println!("{}", a==*b);
+}*/
+
+
+// Rc
+
+
+
+use std::rc::Rc;
+
+fn test(s: Rc<String>) {
+    println!("{}",s);
+}
+
+fn main() {
+    let a = Rc::new(String::from("abc"));
+    let b = a.clone();
+    let c = a.clone();
+    test(b);
+    println!("{}", a);
+    println!("{}", Rc::strong_count(&a));
 }
