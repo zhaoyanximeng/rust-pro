1e24732d7b7e235b704d62caac9c26a58d2cee5e
zhaoyanximeng <1368788830@qq.com>
2023-03-20 00:05:04 +0800

    unwarp处理错误

--- error_handle/src/main.rs
+++ error_handle/src/main.rs
@@ -8,10 +8,27 @@ fn test(i:i32) ->Result<String, i32> {
 }
 
 fn main() {
-   let ret = test(11);
-    if let Ok(s) = ret {
-        println!("成功:{}", s);
-    } else if let Err(i) = ret {
-        println!("失败:{}", i);
-    }
+    let ret = test(3);
+    // if let Ok(s) = ret {
+    //     println!("成功:{}", s);
+    // } else if let Err(i) = ret {
+    //     println!("失败:{}", i);
+    // }
+
+    // match ret {
+    //     Ok(s) => {
+    //         println!("成功:{}", s);
+    //     },
+    //     Err(i) => {
+    //         println!("失败:{}", i);
+    //     }
+    // }
+
+    // 出错给默认值
+    // println!("{}", ret.unwrap_or(("my default value").to_string()));
+
+    // 出错进行业务处理
+    println!("{}", ret.unwrap_or_else(|error|{
+        error.to_string()
+    }))
 }
