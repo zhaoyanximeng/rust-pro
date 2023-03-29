53225b141e6da5aee76b99fce5d78463a1b4e624
zhaoyanximeng <1368788830@qq.com>
2023-03-29 23:30:07 +0800

    非结构体反序列化取值

--- project_demo/src/main.rs
+++ project_demo/src/main.rs
@@ -3,12 +3,21 @@ use lib::prelude::UserInit;
 use lib::user::*;
 
 fn main() {
-   let u = User::new("zyxm");
-    println!("{}", serde_json::to_string(&u).unwrap());
+   // let u = User::new("zyxm");
+   //  println!("{}", serde_json::to_string(&u).unwrap());
    //  let user_str = r#"{
    //      "id": 0,
    //      "user_name": "zyxm"
    //  }"#;
    //  let user:User = serde_json::from_str(user_str).unwrap();
    //  println!("{}", user)
+
+    let user_str = r#"{
+        "id":0,
+        "user_name":"zyxm",
+        "friends":["coleary", "faith"],
+        "roles":[{"name":"admin"},{"name":"zyxm"}]
+    }"#;
+    let user:serde_json::Value = serde_json::from_str(user_str).unwrap();
+    println!("{}", user.as_object().and_then(|v| v.get("friends")).and_then(|v|v.get(0)).unwrap());
 }
