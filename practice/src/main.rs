49a968473721d21b4467ad6f239563572211df24
zhaoyanximeng <1368788830@qq.com>
2023-03-29 21:45:53 +0800

    struct实现display trait

--- practice/src/main.rs
+++ practice/src/main.rs
@@ -4,6 +4,8 @@
 //     println!("{:?},{:?}", a,  b);
 // }
 
+use std::fmt::Formatter;
+
 impl Clone for User{
     fn clone(&self) -> Self {
         User{
@@ -25,8 +27,25 @@ fn load_user(u:&mut User) {
     u.age = 42;
 }
 
+// fn main() {
+//     let mut user = User{id: 123, ..Default::default()};
+//     load_user(&mut user);
+//     println!("a={:?}", user);
+// }
+
+impl User {
+    fn new()->Self {
+        User{..Default::default()}
+    }
+}
+
+impl std::fmt::Display for User {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "用户id是{}，用户年龄是{}", self.id, self.age)
+    }
+}
+
 fn main() {
-    let mut user = User{id: 123, ..Default::default()};
-    load_user(&mut user);
-    println!("a={:?}", user);
+    let u = User::new();
+    println!("{}", u);
 }
