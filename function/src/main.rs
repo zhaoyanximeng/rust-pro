b35e743bc5a094e308b178ef76b390954235d6f2
zhaoyanximeng <1368788830@qq.com>
2023-03-30 22:56:14 +0800

    闭包作为struct字段

--- function/src/main.rs
+++ function/src/main.rs
@@ -5,11 +5,31 @@ fn exec<F> (f: F)
     f();
 }
 
+// struct User<F>
+// where
+// F:Fn()
+// {
+//     id: i32,
+//     info: F,
+// }
+
+struct User<'a>
+{
+    id: i32,
+    info:&'a dyn Fn(),
+}
+
 fn main() {
     let str = String::from("abc");
     let f = || {
         println!("{}", str);
     };
-    exec(f);
-    exec(f);
+    // exec(f);
+    // exec(f);
+
+    // let u = User{id:1, info: f};
+    // (u.info)();
+
+    let u = User{id:1, info: &f};
+    (u.info)();
 }
