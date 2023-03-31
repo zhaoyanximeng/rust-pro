555663fd4c6ac088e4040758850f239a7c21e121
zhaoyanximeng <1368788830@qq.com>
2023-04-01 06:54:10 +0800

    解析query参数

--- actix_example/src/main.rs
+++ actix_example/src/main.rs
@@ -1,13 +1,35 @@
-use actix_web::{App, get, HttpResponse, HttpServer, Responder};
+use actix_web::{App, get, HttpRequest, HttpResponse, HttpServer, Responder, web};
+use web::Query;
+use serde;
+
+#[derive(serde::Deserialize,Debug)]
+struct IndexParams {
+    #[serde(default)]
+    id:i32,
+    #[serde(default="default_name")]
+    name:String
+}
+
+fn default_name()->String{
+    "guest".to_string()
+}
 
 #[get("/")]
-async fn index()->impl Responder {
-    HttpResponse::Ok().body("myindex")
+async fn index(req: HttpRequest)->impl Responder {
+    // let q:Query<IndexParams> = web::Query::from_query(req.query_string()).unwrap();
+    // let q_str = format!("{:?}", q);
+    let qs = qstring::QString::from(req.query_string());
+    let id = qs.get("id").unwrap_or_default();
+    if id == "" {
+        return HttpResponse::BadRequest().body("err params:id");
+    }
+    HttpResponse::Ok().body("ok")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+
         App::new().service(index)
     }).bind(("0.0.0.0", 8080))?.run().await
 }
