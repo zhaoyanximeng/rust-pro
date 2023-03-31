e66ce7ce538620b0a076245a8fbf834a63261fc2
zhaoyanximeng <1368788830@qq.com>
2023-04-01 04:08:17 +0800

    tokio task创建运行

--- tokio_example/src/main.rs
+++ tokio_example/src/main.rs
@@ -1,6 +1,7 @@
 use chrono::Utc;
 use std::time::Duration;
 use tokio::runtime::*;
+use tokio::task;
 use tokio::time::*;
 
 async fn job(i: i32) -> i32 {
@@ -25,17 +26,45 @@ async fn job(i: i32) -> i32 {
 //     println!("耗时:{}", (end_time-start_time).num_milliseconds());
 // }
 
-#[tokio::main(flavor = "multi_thread")]
+// #[tokio::main(flavor = "multi_thread")]
+// async fn main() {
+//     let handler = tokio::runtime::Handle::current();
+//     let h1 = handler.spawn(job(123));
+//     let h2 = handler.spawn(job(234));
+//     let h3 = handler.spawn(job(456));
+//     let start_time = Utc::now().time();
+//     let r1 = h1.await.unwrap();
+//     let r2 = h2.await.unwrap();
+//     let r3 = h3.await.unwrap();
+//     println!("执行结果是:{},{},{}", r1, r2, r3);
+//     let end_time = Utc::now().time();
+//     println!("耗时:{}", (end_time - start_time).num_milliseconds());
+// }
+
+#[tokio::main]
 async fn main() {
-    let handler = tokio::runtime::Handle::current();
-    let h1 = handler.spawn(job(123));
-    let h2 = handler.spawn(job(234));
-    let h3 = handler.spawn(job(456));
-    let start_time = Utc::now().time();
-    let r1 = h1.await.unwrap();
-    let r2 = h2.await.unwrap();
-    let r3 = h3.await.unwrap();
-    println!("执行结果是:{},{},{}", r1, r2, r3);
-    let end_time = Utc::now().time();
-    println!("耗时:{}", (end_time - start_time).num_milliseconds());
+
+    let h1 = task::spawn(async {
+        sleep(Duration::from_secs(5)).await;
+        // println!("{}", "h1");
+        println!("h1已经完成了");
+        "h1"
+    });
+
+    let h2 = task::spawn(async {
+        sleep(Duration::from_secs(2)).await;
+        // println!("{}", "h2");
+        println!("h2已经完成了");
+        "h2"
+    });
+
+    // h1.await.unwrap();
+    // h2.await.unwrap();
+    // let (_,serde_json) = tokio::join!(h1, h2);
+    let ret1 = h1.await.unwrap();
+    println!("{}", ret1);
+    let ret2 = h2.await.unwrap();
+    println!("{}", ret2);
+
+    println!("{}", "abc");
 }
