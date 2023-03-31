66c1ae1266d3263a9fb8ea0d3f1b869da9347e36
zhaoyanximeng <1368788830@qq.com>
2023-04-01 03:36:12 +0800

    使用spawn创建异步任务

--- tokio_example/src/main.rs
+++ tokio_example/src/main.rs
@@ -13,11 +13,14 @@ async fn job(i:i32)->i32 {
 
 fn main() {
     let rt = Builder::new_multi_thread().worker_threads(2).enable_time().build().unwrap();
+    let h1 = rt.spawn(job(123));
+    let h2 = rt.spawn(job(234));
+    let h3 = rt.spawn(job(456));
     let start_time = Utc::now().time();
     rt.block_on(async{
-        let r1 = job(123).await;
-        let r2 = job(234).await;
-        let r3 = job(345).await;
+        let r1 = h1.await.unwrap();
+        let r2 = h2.await.unwrap();
+        let r3 = h3.await.unwrap();
         println!("执行结果是:{},{},{}", r1, r2, r3);
     });
     let end_time = Utc::now().time();
